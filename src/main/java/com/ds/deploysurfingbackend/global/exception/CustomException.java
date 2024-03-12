fab29ff39b95d111ab0d61f6b410cde59dccbc74
DDonghyeo <gsu9045000@gmail.com>
2024-03-12 14:04:56 +0900

    feat: Error Handler & Exception

---
+++ src/main/java/com/ds/deploysurfingbackend/global/exception/CustomException.java
@@ -0,0 +1,21 @@
+package com.ds.deploysurfingbackend.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String errorMessage;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorCode.getDetail();
+    }
+
+    //@Valid 오류처리
+    public CustomException(ErrorCode errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+}
