9d2d721ade6b8d5256d85a0d33fba3372f44d097
DDonghyeo <gsu9045000@gmail.com>
2024-03-12 13:55:31 +0900

    Project Initialization

---
+++ src/main/java/com/ds/deploysurfingbackend/DeploySurfingBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ds.deploysurfingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySurfingBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploySurfingBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ds/deploysurfingbackend/DeploySurfingBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.deploysurfingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySurfingBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
