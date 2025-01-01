aa03b8169c252f27465ff8ccb0c6146fde3e86c2
DDonghyeo <gsu9045000@gmail.com>
2025-01-01 22:36:45 +0900

    refactor : Repository Timeout 설정

--- src/main/java/com/ds/deploysurfingbackend/global/event/OutboxEventRepository.java
+++ src/main/java/com/ds/deploysurfingbackend/global/event/OutboxEventRepository.java
@@ -2,10 +2,12 @@ package com.ds.deploysurfingbackend.global.event;
 
 import com.ds.deploysurfingbackend.global.event.entity.OutBoxEvent;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,8 @@ import java.util.List;
 
 public interface OutboxEventRepository extends JpaRepository<OutBoxEvent, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE) //비관적 락 사용
+    @Lock(LockModeType.PESSIMISTIC_WRITE) //비관 배타락
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")}) //락 타임아웃 3s
     @Query("""
             SELECT e FROM OutBoxEvent e
             WHERE (e.status = :status OR
